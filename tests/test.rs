372e474c46c613752618814b7f5a23b2955b9a45
Tristan Guichaoua <tguichaoua.dev@gmail.com>
2025-02-16 14:59:13 +0100

    🚚 merge test files

--- tests/test.rs
+++ tests/test.rs
@@ -32,6 +32,59 @@ fn patch() {
 }
 
 #[test]
+fn patch_deep() {
+    #[derive(Patch, PartialEq, Eq, Debug)]
+    struct Foo {
+        a: u32,
+        #[patch(with = GooPatch)]
+        b: Goo,
+        #[patch(with = HooPatch)]
+        c: Hoo,
+    }
+
+    #[derive(Patch, PartialEq, Eq, Debug)]
+    struct Goo {
+        a: bool,
+        b: u8,
+    }
+
+    #[derive(Patch, PartialEq, Eq, Debug)]
+    struct Hoo {
+        a: String,
+    }
+
+    let mut value = Foo {
+        a: 12,
+        b: Goo { a: true, b: 65 },
+        c: Hoo {
+            a: String::from("Hello"),
+        },
+    };
+
+    value.patch(FooPatch {
+        b: GooPatch {
+            b: Some(100),
+            ..Default::default()
+        },
+        c: HooPatch {
+            a: Some(String::from("World!")),
+        },
+        ..Default::default()
+    });
+
+    assert_eq!(
+        value,
+        Foo {
+            a: 12,
+            b: Goo { a: true, b: 100 },
+            c: Hoo {
+                a: String::from("World!"),
+            },
+        }
+    );
+}
+
+#[test]
 fn from_impl() {
     #[derive(Patch, Debug, PartialEq, Eq)]
     #[patch(from, attr = derive(Debug, PartialEq, Eq))]
